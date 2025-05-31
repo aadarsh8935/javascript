2858680005b4e7f2b27e77f4a0b413808d766c5f
aadarsh8935 <aadarshraj255@gmail.com>
2025-05-31 18:57:14 +0530

    javascript

---
+++ add button.js
@@ -0,0 +1,8 @@
+function fun(){
+    let a = parseInt(prompt("Enter any number"))
+    let b = parseInt(prompt("Enter any number"))
+    console.log(a+b);
+}
+function color(cl){
+    document.body.style.backgroundColor=cl
+}
